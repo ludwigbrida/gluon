5406a5413abf2482060a5acb34132a9e6897ae70
Ludwig Brida <ludwig@ludwigbrida.dev>
2026-09-22 16:25:13 +0200

    feat(wgpu): create rectangle pipeline

--- crates/gluon-wgpu/src/renderer.rs
+++ crates/gluon-wgpu/src/renderer.rs
@@ -1,11 +1,52 @@
 use gluon_core::DisplayList;
-use wgpu::{CommandEncoder, Device, TextureFormat, TextureView};
+use wgpu::{
+  BlendState, ColorTargetState, ColorWrites, CommandEncoder, Device, FragmentState,
+  MultisampleState, PrimitiveState, RenderPipeline, RenderPipelineDescriptor,
+  ShaderModuleDescriptor, ShaderSource, TextureFormat, TextureView, VertexState,
+};
 
-pub struct Renderer;
+pub struct Renderer {
+  pipeline: RenderPipeline,
+}
 
 impl Renderer {
-  pub fn new(_device: &Device, _target_format: TextureFormat) -> Self {
-    Self
+  pub fn new(device: &Device, target_format: TextureFormat) -> Self {
+    let shader_module_descriptor = ShaderModuleDescriptor {
+      label: Some("gluon_shader_module"),
+      source: ShaderSource::Wgsl(include_str!("rect.wgsl").into()),
+    };
+
+    let shader_module = device.create_shader_module(shader_module_descriptor);
+
+    let pipeline_descriptor = &RenderPipelineDescriptor {
+      label: Some("gluon_pipeline"),
+      layout: None,
+      vertex: VertexState {
+        module: &shader_module,
+        entry_point: Some("vertex_main"),
+        compilation_options: Default::default(),
+        buffers: &[],
+      },
+      primitive: PrimitiveState::default(),
+      depth_stencil: None,
+      multisample: MultisampleState::default(),
+      fragment: Some(FragmentState {
+        module: &shader_module,
+        entry_point: Some("fragment_main"),
+        compilation_options: Default::default(),
+        targets: &[Some(ColorTargetState {
+          format: target_format,
+          blend: Some(BlendState::ALPHA_BLENDING),
+          write_mask: ColorWrites::ALL,
+        })],
+      }),
+      multiview_mask: None,
+      cache: None,
+    };
+
+    let pipeline = device.create_render_pipeline(pipeline_descriptor);
+
+    Self { pipeline }
   }
 
   pub fn render(
